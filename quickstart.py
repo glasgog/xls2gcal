b6e0cb3d65ac0f3b4495008756940fa6ade5cf75
Ilario Digiacomo <glasgo.g@gmail.com>
2017-05-21 14:39:11 +0200

    Code and algorithm fix
    + Import call fixed
    + GCal instanced only one time now
    + Calendar name passed as GCal init argument
    + Max result is a GCal class parameter now
    + PEP8 style implemented
    + License updated

--- quickstart.py
+++ quickstart.py
@@ -9,6 +9,7 @@ from oauth2client import tools
 from oauth2client.file import Storage
 
 from datetime import datetime
+import dateutil.parser
 
 try:
     import argparse
@@ -16,10 +17,11 @@ try:
 except ImportError:
     flags = None
 
-# If modifying these scopes, delete your previously saved credentials
+# If modify these scopes, delete your previously saved credentials
 # at ~/.credentials/calendar-python-quickstart.json
-#SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
-SCOPES = 'https://www.googleapis.com/auth/calendar' #NOTE: scope modified because i want to write too
+# SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
+# NOTE: scope modified because i want to write too
+SCOPES = 'https://www.googleapis.com/auth/calendar'
 CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'Google Calendar API Python Quickstart'
 
@@ -31,7 +33,7 @@ def get_credentials():
     the OAuth2 flow is completed to obtain the new credentials.
 
     Returns:
-        Credentials, the obtained credential.
+            Credentials, the obtained credential.
     """
     home_dir = os.path.expanduser('~')
     credential_dir = os.path.join(home_dir, '.credentials')
@@ -47,54 +49,58 @@ def get_credentials():
         flow.user_agent = APPLICATION_NAME
         if flags:
             credentials = tools.run_flow(flow, store, flags)
-        else: # Needed only for compatibility with Python 2.6
+        else:  # Needed only for compatibility with Python 2.6
             credentials = tools.run(flow, store)
         print('Storing credentials to ' + credential_path)
     return credentials
 
 
 class GCal:
+    # scelgo il nome del calendario che mi interessa (NOTA: case ensitive)
+    # da rimuovere quando verra' implementata la possibilita' di creare un
+    # nuovo calendario
+    CALENDAR = "test"
+    MAX_RESULTS = 10  # 365
 
     credentials = None
     service = None
-    #scelgo il nome del calendario che mi interessa (NOTA: case sensitive)
-    #e cerco il suo id
-    CALENDAR = "test"   
     calendar_id = None
 
-    def __init__(self):
+    def __init__(self, calendar_name=CALENDAR):
         """Shows basic usage of the Google Calendar API.
 
         Creates a Google Calendar API service object and outputs a list of the next
-        10 events on the user's calendar.
+        MAX_RESULTS events on the user's calendar.
         """
         self.credentials = get_credentials()
         http = self.credentials.authorize(httplib2.Http())
         self.service = discovery.build('calendar', 'v3', http=http)
 
-        #cerco l'id relativo al nome del calendario che mi interessa
-        print('Getting the calendar list (name and id):')
+        # cerco l'id relativo al nome del calendario che mi interessa
+        print('Getting the calendar list (name and id)...')
         page_token = None
         while True:
             calendar_list = self.service.calendarList().list(pageToken=page_token).execute()
             for calendar_list_entry in calendar_list['items']:
-                print ("-", calendar_list_entry['summary'], "->" , calendar_list_entry['id'])
-                if calendar_list_entry['summary'] == self.CALENDAR:
+                print("-", calendar_list_entry['summary'],
+                      "->", calendar_list_entry['id'])
+                # Cerco l'id di calendar_name
+                if calendar_list_entry['summary'] == calendar_name:
                     self.calendar_id = calendar_list_entry['id']
             page_token = calendar_list.get('nextPageToken')
             if not page_token:
                 break
-
-        #se non trovo il calendario che mi interessa, seleziono
-        #l'id del calendario principale
+        # se non trovo il calendario che mi interessa, seleziono
+        # l'id del calendario principale
         if self.calendar_id == None:
-            self.calendar_id = "primary" #
-        print ("Using id:", self.calendar_id)
+            self.calendar_id = "primary"
+        print("Using id:", self.calendar_id)
+        print()
 
-        now = datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-        print('Getting the upcoming 10 events')
+        now = datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+        print('Getting the upcoming', self.MAX_RESULTS, 'events...')
         eventsResult = self.service.events().list(
-            calendarId=self.calendar_id, timeMin=now, maxResults=10, singleEvents=True,
+            calendarId=self.calendar_id, timeMin=now, maxResults=self.MAX_RESULTS, singleEvents=True,
             orderBy='startTime').execute()
         events = eventsResult.get('items', [])
 
@@ -103,132 +109,137 @@ class GCal:
         for event in events:
             start = event['start'].get('dateTime', event['start'].get('date'))
             print("-", start, event['summary'])
+        print()
 
     def add_event(self, name="Work", start="2020-05-08T09:00:00+02:00", end="2020-05-08T09:30:00+02:00"):
         """
         Crea l'evento da aggiungere al calendario.
-		E' preferibile che gli argomenti siano di tipo datatime:
-		fare in ogni caso attenzione che siano utc o comunque correttamente localizzati
-		(nota che l'offset e' +01:00, mentre quando si e' in DST e' +02:00)
+        E' preferibile che gli argomenti siano di tipo datatime:
+        fare in ogni caso attenzione che siano utc o comunque correttamente localizzati
+        (nota che l'offset e' +01:00, mentre quando si e' in DST e' +02:00)
         """
-        
-        if type(start) == datetime and type(end) == datetime: #le formatto correttamente se non lo sono gia'
-	        start=start.isoformat('T')
-	        end=end.isoformat('T')
+
+        # le formatto correttamente se non lo sono gia'
+        if type(start) == datetime and type(end) == datetime:
+            start = start.isoformat('T')
+            end = end.isoformat('T')
 
         event = {
-        'summary': name,
-        'location': 'Test location',
-        'description': 'Test description',
-        'start': {
-        'dateTime': start,
-        'timeZone': 'Europe/Rome',
-        },
-        'end': {
-        'dateTime': end,
-        'timeZone': 'Europe/Rome',
-        },
-        'reminders': {
-        'useDefault': False,
-        'overrides': [
-        {'method': 'popup', 'minutes': 10},
-        ],
-        },
+            'summary': name,
+            'location': 'Test location',
+            'description': 'Test description',
+            'start': {
+                'dateTime': start,
+                'timeZone': 'Europe/Rome',
+            },
+            'end': {
+                'dateTime': end,
+                'timeZone': 'Europe/Rome',
+            },
+            'reminders': {
+                'useDefault': False,
+                'overrides': [
+                    {'method': 'popup', 'minutes': 10},
+                ],
+            },
         }
 
-        event = self.service.events().insert(calendarId=self.calendar_id, body=event).execute()
-        print ('Event created: %s' % (event.get('htmlLink')))
+        event = self.service.events().insert(
+            calendarId=self.calendar_id, body=event).execute()
+        print('Event created: %s' % (event.get('htmlLink')))
+        print()
 
     def event_on_date(self, date):
         """ Check if in a given date there is already an event"""
-        now = datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-        print('event_on_date: Getting the upcoming 10 events')
+        now = datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+        print('event_on_date: Getting the upcoming',
+              self.MAX_RESULTS, 'events...')
         eventsResult = self.service.events().list(
-            calendarId=self.calendar_id, timeMin=now, maxResults=10, singleEvents=True,
-            orderBy='startTime').execute() #NOTE: only the first 10 events are getted
+            calendarId=self.calendar_id, timeMin=now, maxResults=self.MAX_RESULTS, singleEvents=True,
+            orderBy='startTime').execute()  # NOTE: only the first 10 events are getted
         events = eventsResult.get('items', [])
-        
+
         if not events:
-            print('No upcoming events found.')
+            print('- No upcoming events found.')
         for event in events:
             start = event['start'].get('dateTime', event['start'].get('date'))
-            print("-", start, event['summary'])
+            # print("-", start, event['summary'])
 
-            #convert from google api format
-            import dateutil.parser #HARDCODED
-            dt = dateutil.parser.parse(start)
-            if dt.date() == date.date():
-                print("Event found")
+            # convert from google api format
+            start_dt = dateutil.parser.parse(start)
+            if start_dt.date() == date.date():
+                print("- Event found:", start, event['summary'])
                 return True
-        return False        
+        return False
 
     def update_event(self, date, new_name="Work", new_start="2017-05-08T09:00:00+02:00", new_end="2017-05-08T09:30:00+02:00"):
-    	#se avessi a disposizione l'id potrei evitare di rieffettuare la ricerca
-    	if type(new_start) == datetime and type(new_end) == datetime: #le formatto correttamente se non lo sono gia'
-	        new_start=new_start.isoformat('T')
-	        new_end=new_end.isoformat('T')
+        # se avessi a disposizione l'id potrei evitare di rieffettuare la
+        # ricerca
+        # le formatto correttamente se non lo sono gia'
+        if type(new_start) == datetime and type(new_end) == datetime:
+            new_start = new_start.isoformat('T')
+            new_end = new_end.isoformat('T')
 
-        now = datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-        print('Getting the upcoming 10 events')
+        now = datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+        print('update_event: Getting the upcoming',
+              self.MAX_RESULTS, 'events...')
         eventsResult = self.service.events().list(
-            calendarId=self.calendar_id, timeMin=now, maxResults=10, singleEvents=True,
-            orderBy='startTime').execute() #NOTE: only the first 10 events are getted
+            calendarId=self.calendar_id, timeMin=now, maxResults=self.MAX_RESULTS, singleEvents=True,
+            orderBy='startTime').execute()  # NOTE: only the first 10 events are getted
         events = eventsResult.get('items', [])
-        
+
         if not events:
-            print('No upcoming events found.')
+            print('- No upcoming events found.')
         for event in events:
             start = event['start'].get('dateTime', event['start'].get('date'))
-            print("-", start, event['summary'])
+            # print("-", start, event['summary'])
 
-            #convert from google api format
-            import dateutil.parser #HARDCODED
-            dt = dateutil.parser.parse(start)
-            if dt.date() == date.date():
-                print("Event found")
+            # convert from google api format
+            start_dt = dateutil.parser.parse(start)
+            if start_dt.date() == date.date():
+                print("- Event found:", start, event['summary'])
                 event["summary"] = new_name
-                event["start"] = {'dateTime': new_start, 'timeZone': 'Europe/Rome',}
-                event["end"] = {'dateTime': new_end, 'timeZone': 'Europe/Rome',}
-                updated_event = self.service.events().update(calendarId=self.calendar_id, eventId=event['id'], body=event).execute()
+                event["start"] = {'dateTime': new_start,
+                                  'timeZone': 'Europe/Rome', }
+                event["end"] = {'dateTime': new_end,
+                                'timeZone': 'Europe/Rome', }
+                updated_event = self.service.events().update(calendarId=self.calendar_id,
+                                                             eventId=event['id'], body=event).execute()
                 # Print the updated date.
-                print(updated_event['updated'])
+                print("  Event updated:", updated_event['updated'])
                 return True
         return False
 
     def delete_event(self, date):
 
-    	now = datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-        print('Getting the upcoming 10 events')
+        now = datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+        print('delete_event: Getting the upcoming',
+              self.MAX_RESULTS, 'events...')
         eventsResult = self.service.events().list(
-            calendarId=self.calendar_id, timeMin=now, maxResults=10, singleEvents=True,
-            orderBy='startTime').execute() #NOTE: only the first 10 events are getted
+            calendarId=self.calendar_id, timeMin=now, maxResults=self.MAX_RESULTS, singleEvents=True,
+            orderBy='startTime').execute()  # NOTE: only the first 10 events are getted
         events = eventsResult.get('items', [])
 
         if not events:
-            print('No upcoming events found.')
+            print('- No upcoming events found.')
         for event in events:
             start = event['start'].get('dateTime', event['start'].get('date'))
-            print("-", start, event['summary'])
+            # print("-", start, event['summary'])
 
-        	#convert from google api format
-            import dateutil.parser #HARDCODED
-            dt = dateutil.parser.parse(start)
-            if dt.date() == date.date():
-                print("Event found")
-                self.service.events().delete(calendarId=self.calendar_id, eventId=event['id']).execute()
-                print("Event deleted.")
+            # convert from google api format
+            start_dt = dateutil.parser.parse(start)
+            if start_dt.date() == date.date():
+                print("- Event found:", start, event['summary'])
+                self.service.events().delete(calendarId=self.calendar_id,
+                                             eventId=event['id']).execute()
+                print("  Event deleted.")
                 return True
-		return False
-
-
-
-
+        return False
 
 
 def main():
-    #TOFIX when class is modified
-    c = GCal()
-    c.add_event()
+    # TOFIX when class is modified
+    print("Main is stil not implemented.")
 
 if __name__ == '__main__':
     main()
--- readxls.py
+++ readxls.py
@@ -1,3 +1,147 @@
+from xlrd import open_workbook  # excel reading
+from datetime import datetime, date, time, timedelta
+import pytz  # timezone definitions
+import quickstart
+
+SHEET_INDEX = 0
+FIRST_ROW_INDEX = 1
+WORKER_INDEX = 1  # column where the worker is
+CALENDAR = "test"
+
+
+def month_int_from_str(month):
+    year = {"gen": 1,
+            "feb": 2,
+            "mar": 3,
+            "apr": 4,
+            "mag": 5,
+            "giu": 6,
+            "lug": 7,
+            "ago": 8,
+            "set": 9,
+            "ott": 10,
+            "nov": 11,
+            "dic": 12}
+    return year[month]
+
+
+def shift_str_from_char(shift):
+    work = {"G": "Giorno",
+            "M": "Mattina",
+            "P": "Pomeriggio",
+            "N": "Notte"}
+    if shift not in work:
+        return
+    return work[shift]
+
+wb = open_workbook('esempio.xlsx')
+sheet = wb.sheet_by_index(SHEET_INDEX)  # HARDCODED
+
+# fill a dictionary workday = {"1-gen": "G", ..., "date": "shift", ...}
+# i can access a shift with workday["1-gen"]
+workday = {}
+for row in xrange(FIRST_ROW_INDEX, sheet.nrows):  # HARDCODED
+    workday[sheet.cell(row, SHEET_INDEX).value] = sheet.cell(
+        row, WORKER_INDEX).value
+# print " 1-gen is " + workday["1-gen"]
+
+c = quickstart.GCal(CALENDAR)
+
+for d in workday:
+
+    # print str(str(d).split("-")) + " " + str(workday[d])
+
+    # d is of the kind 8-mag. I want 2017-05-08
+    day_month_lst = d.split("-")
+    day = int(day_month_lst[0])
+    month = month_int_from_str(day_month_lst[1])
+    year = 2017  # <----------------------------------------------------------------HARDCODED!
+    shift = shift_str_from_char(workday[d])  # remember: d is the key
+
+    if shift:
+        # get the full date, with the shift start time
+        print str(day) + "/" + str(month) + "/" + str(year) + ": " + str(shift)
+
+        shift_time = {"G": time(9, 0),
+                      "M": time(6, 0),
+                      "P": time(14, 0),
+                      "N": time(22, 0)}
+        dt = datetime.combine(date(year, month, day), shift_time[workday[d]])
+        # print "Datatime: " + str(dt)
+        # I need UTC date
+        local = pytz.timezone("Europe/Rome")
+        local_dt = local.localize(dt, is_dst=None)
+        print " Local datatime: " + str(local_dt)
+        # utc_dt = local_dt.astimezone(pytz.utc) # NOTE: local_dt==utc_dt
+        # format needed for google api: 2017-05-08T09:00:00+02:00 # utc_string
+        # = local_dt.isoformat('T')
+
+        if not c.event_on_date(local_dt):
+            c.add_event(shift, local_dt, local_dt + timedelta(hours=9))
+        else:
+            # dovrei restituire l'id dell'evento per evitare di effettuare nuovamente la ricerca
+            #c.update_event(local_dt,(local_dt+timedelta(hours=1)), (local_dt+timedelta(hours=10)))
+            c.update_event(local_dt, shift, local_dt,
+                           local_dt + timedelta(hours=9))
+        print
+    else:
+        print str(day) + "/" + str(month) + "/" + str(year) + ": Riposo"
+        dt = datetime.combine(date(year, month, day),
+                              time(8, 0))  # l'ora e' fittizia
+        # print "Datatime: " + str(dt)
+        # I need UTC date
+        local = pytz.timezone("Europe/Rome")
+        local_dt = local.localize(dt, is_dst=None)
+        print " Local datatime: " + str(local_dt)
+
+        c.delete_event(local_dt)
+        print
+
+
+"""
+Test section beginning:
+	get the last event and add it to the calendar (end 9 hours after the start).
+	if there is already an event on the same day (note: day, not date),
+	this one is updated modifying the hour (in the test is shifted of 1 hour).
+	No work day is still missing.
+"""
+
+
+def localize(date_time):
+    import pytz
+    local = pytz.timezone("Europe/Rome")
+    local_dt = local.localize(date_time, is_dst=None)
+    return local_dt
+
+
+def gdate(date_time):
+    # convert to google api format
+    local_dt = localize(date_time)
+    return local_dt.isoformat('T')
+
+
+def un_gdate(gdate):
+    # convert from google api format
+    import dateutil.parser
+    dt = dateutil.parser.parse(gdate)
+    return dt
+
+
+# import quickstart
+# from datetime import timedelta
+
+# print
+# c = quickstart.GCal()
+# if not c.event_on_date(local_dt):
+# 	print "Add the new event"
+# 	c.add_event(local_dt, local_dt+datetime.timedelta(hours=9))
+# else:
+# 	#dovrei restituire l'id dell'evento per evitare di effettuare nuovamente la ricerca
+# 	print "Updating event"
+# 	c.update_event(local_dt,(local_dt+datetime.timedelta(hours=1)), (local_dt+datetime.timedelta(hours=10)))
+
+#-----------------------------------
+#
 # from xlrd import open_workbook
 # wb = open_workbook('esempio.xlsx')
 # for s in wb.sheets():
@@ -15,145 +159,3 @@
 
 # for row in values:
 # 	print str(row[0]) + " -> " + str(row[1])
-
-#-----------------------------------
-
-SHEET_INDEX = 0
-FIRST_ROW_INDEX = 1
-WORKER_INDEX = 1 #column where the worker is
-
-def month_int_from_str(month):
-	year = {"gen": 1, \
-			"feb": 2, \
-			"mar": 3, \
-			"apr": 4, \
-			"mag": 5, \
-			"giu": 6, \
-			"lug": 7, \
-			"ago": 8, \
-			"set": 9, \
-			"ott": 10, \
-			"nov": 11, \
-			"dic": 12}
-	return year[month]
-
-def shift_str_from_char(shift):
-	work = {"G": "Giorno", \
-			"M": "Mattina", \
-			"P": "Pomeriggio", \
-			"N": "Notte"}
-	if shift not in work:
-		return
-	return work[shift]
-
-from xlrd import open_workbook
-wb = open_workbook('esempio.xlsx')
-sheet = wb.sheet_by_index(SHEET_INDEX) #HARDCODED
-
-#fill a dictionary workday = {"1-gen": "G", ..., "date": "shift", ...}
-#i can access a shift with workday["1-gen"]
-workday = {}
-for row in xrange(FIRST_ROW_INDEX, sheet.nrows): #HARDCODED
-    workday[sheet.cell(row,SHEET_INDEX).value] = sheet.cell(row,WORKER_INDEX).value
-#print " 1-gen is " + workday["1-gen"]
-
-for d in workday:
-
-	#print str(str(d).split("-")) + " " + str(workday[d])
-
-	#d is of the kind 8-mag. I want 2017-05-08
-	day_month_lst = d.split("-")
-	day = int(day_month_lst[0])
-	month = month_int_from_str(day_month_lst[1])
-	year = 2017 #HARDCODED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-	shift = shift_str_from_char(workday[d]) #remember: d is the key
-
-	if shift:
-		#get the full date, with the shift start time
-		print str(day) + "/" + str(month) + "/" + str(year) + ": " + str(shift)
-
-		from datetime import datetime, date, time #TOFIX: obviously import at the beginning
-		shift_time = {"G": time(9,0), \
-						"M": time(6,0), \
-						"P": time(14,0), \
-						"N": time(22,0)}
-		dt = datetime.combine(date(year,month,day), shift_time[workday[d]])
-		print "Datatime: " + str(dt)
-
-		#I need UTC date
-		import pytz
-		local = pytz.timezone("Europe/Rome")
-		local_dt = local.localize(dt, is_dst=None)
-		print "Local datatime: " + str(local_dt)
-		# utc_dt = local_dt.astimezone(pytz.utc) # NOTE: local_dt==utc_dt
-		# format needed for google api: 2017-05-08T09:00:00+02:00 # utc_string = local_dt.isoformat('T')
-
-		print
-
-		""" """
-		import quickstart
-		from datetime import timedelta
-
-		c = quickstart.GCal()
-		if not c.event_on_date(local_dt):
-			print "Add the new event"
-			c.add_event(shift, local_dt, local_dt+timedelta(hours=9)) #NOTA: manca ancora il nome
-		else:
-			#dovrei restituire l'id dell'evento per evitare di effettuare nuovamente la ricerca
-			print "Updating event"
-			#c.update_event(local_dt,(local_dt+timedelta(hours=1)), (local_dt+timedelta(hours=10)))
-			c.update_event(local_dt, shift, local_dt, local_dt+timedelta(hours=9))
-	else:
-		from datetime import datetime, date, time
-		dt = datetime.combine(date(year,month,day),time(8,0)) #l'ora e' fittizia
-		print "Datatime: " + str(dt)
-		#I need UTC date
-		import pytz
-		local = pytz.timezone("Europe/Rome")
-		local_dt = local.localize(dt, is_dst=None)
-		print "Local datatime: " + str(local_dt)
-
-		import quickstart
-		c = quickstart.GCal()
-		c.delete_event(local_dt)
-
-
-
-"""
-Test section beginning:
-	get the last event and add it to the calendar (end 9 hours after the start).
-	if there is already an event on the same day (note: day, not date),
-	this one is updated modifying the hour (in the test is shifted of 1 hour).
-	No work day is still missing.
-"""
-
-def localize(date_time):
-	import pytz
-	local = pytz.timezone("Europe/Rome")
-	local_dt = local.localize(date_time, is_dst=None)
-	return local_dt
-
-def gdate(date_time):
-	#convert to google api format
-	local_dt = localize(date_time)
-	return local_dt.isoformat('T')
-
-def un_gdate(gdate):
-	#convert from google api format
-	import dateutil.parser
-	dt = dateutil.parser.parse(gdate)
-	return dt
-
-
-# import quickstart
-# from datetime import timedelta
-
-# print
-# c = quickstart.GCal()
-# if not c.event_on_date(local_dt):
-# 	print "Add the new event"
-# 	c.add_event(local_dt, local_dt+datetime.timedelta(hours=9))
-# else:
-# 	#dovrei restituire l'id dell'evento per evitare di effettuare nuovamente la ricerca
-# 	print "Updating event"
-# 	c.update_event(local_dt,(local_dt+datetime.timedelta(hours=1)), (local_dt+datetime.timedelta(hours=10)))
